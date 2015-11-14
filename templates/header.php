0b25f3b6dd353481ee3c985a59522fe299cee3fa
Lou Griffith <griffithlou@gmail.com>
2015-11-14 09:10:14 -0600

    Added Walker to Sage

--- templates/header.php
+++ templates/header.php
@@ -1,15 +1,33 @@
 <header class="banner">
   <div class="container">
-    <a class="brand" href="<?= esc_url(home_url('/')); ?>">
-      <i class="icon-1color"></i>
-      <span><?php bloginfo('name'); ?></span>
-    </a>
-    <nav class="nav-primary">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
-      endif;
-      ?>
-    </nav>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+        
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        
+        <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>">
+          <i class="icon-1color"></i>
+          <span><?php bloginfo('name'); ?></span>
+        </a>
+
+      </div>
+    
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <?php
+          if (has_nav_menu('primary_navigation')) :
+            wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav navbar-right']);
+          endif;
+          ?>
+        </div>
+    </div>
+  </nav>
   </div>
 </header>
