e5b172415df1959e3a059fd36d8f5352780373de
Lou Griffith <griffithlou@gmail.com>
2015-11-14 14:01:56 -0600

    Adding style & content fields for Aperture One Page

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,24 @@
-<footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
-  </div>
+<footer class="global-footer">
+    <div class="container">
+		<div class="footer-left">
+			<a class="footer-brand" href="<?= esc_url(home_url('/')); ?>">
+	          <i class="icon-1color"></i>
+	          <span><?php bloginfo('name'); ?></span>
+	        </a>
+		</div>
+
+		<div class="footer-right">
+			<?php function auto_copyright($year = 'auto'){ ?>
+			   <?php if(intval($year) == 'auto'){ $year = date('Y'); } ?>
+			   <?php if(intval($year) == date('Y')){ echo intval($year); } ?>
+			   <?php if(intval($year) < date('Y')){ echo intval($year) . ' - ' . date('Y'); } ?>
+			   <?php if(intval($year) > date('Y')){ echo date('Y'); } ?>
+			<?php } ?>
+
+			<p>
+				<span class="copyright">Copyright © <?php auto_copyright(); // 2011?> <?php bloginfo('name'); ?>,</span>
+				<span class="reserved">&nbsp; All Rights Reserved.</span>
+			</p>
+		</div>
+	</div>
 </footer>
--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,6 @@
 <header class="banner">
   <div class="container">
-  <nav class="navbar navbar-default">
+  <nav class="navbar navbar-clear">
     <div class="container-fluid">
       <!-- Brand and toggle get grouped for better mobile display -->
       <div class="navbar-header">
